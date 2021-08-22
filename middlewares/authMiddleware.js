8b0af65e88b500dbec473d4722a4cc50fa05b980
[Ahmed <[shaban7642]>
2021-08-22 20:29:49 +0200

    user controller

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,43 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/User';
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token;
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.id).select('-password');
+            next();
+        } catch (error) {
+            res.status(401);
+            throw new Error('Not authorized, token failed');
+        }
+    }
+});
+
+const admin = (req, res, next) => {
+    if (req.user && req.user.role === 'admin') {
+        next();
+    } else {
+        res.status(401);
+        throw new Error('Not authorized as an admin');
+    }
+};
+
+const seller = (req, res, next) => {
+    if (req.user && req.user.role === 'seller') {
+        next();
+    } else {
+        res.status(401);
+        throw new Error('Not authorized as an seller');
+    }
+};
+
+export { protect, admin, seller };
--- server.js
+++ server.js
@@ -3,12 +3,16 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 
 import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(
