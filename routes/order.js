34ba5644363f46464bb23e0b036f79027e056bb2
[Ahmed <[shaban7642]>
2021-09-18 13:00:55 +0200

    prepare for deployment

--- routes/order.js
+++ routes/order.js
@@ -1,20 +1,23 @@
 import express from 'express';
 import {
-	getOrders,
-	getSellerProfit,
-	updateOrderToPaidProfit,
+    getOrders,
+    getSellerProfit,
+    updateOrderToPaidProfit,
 } from '../controllers/Order/admin.js';
 import {
-	addOrderItems,
-	getMyOrders,
-	getOrderById,
-	updateOrderToPaid,
+    addOrderItems,
+    createExcelSheet,
+    getMyOrders,
+    getOrderById,
+    sendExcelSheet,
+    updateOrderToPaid,
 } from '../controllers/Order/order.js';
 import {
-	deleteOrder,
-	getSellerCustomers,
-	getSellerOrders,
-	updateOrderToDelivered,
+    deleteOrder,
+    getSellerCustomers,
+    getSellerOrders,
+    updateOrderStatus,
+    updateOrderToDelivered,
 } from '../controllers/Order/seller.js';
 import { updateOrderPrice } from '../controllers/Order/useOrderCopon.js';
 import { protect, seller, admin } from '../middlewares/authMiddleware.js';
@@ -27,12 +30,15 @@ router.route('/sellercustomers').get(protect, seller, getSellerCustomers);
 router.route('/:id/sellerprofit').get(protect, admin, getSellerProfit);
 router.route('/myorders').get(protect, getMyOrders);
 router
-	.route('/:id')
-	.get(protect, getOrderById)
-	.delete(protect, seller, deleteOrder);
+    .route('/:id')
+    .get(protect, getOrderById)
+    .delete(protect, seller, deleteOrder);
 router.route('/:id/usecopon').put(protect, updateOrderPrice);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
 router.route('/:id/payprofit').put(protect, admin, updateOrderToPaidProfit);
 router.route('/:id/deliver').put(protect, seller, updateOrderToDelivered);
+router.route('/:id/status').put(protect, seller, updateOrderStatus);
+router.route('/:id/excelsheetcreate').post(protect, seller, createExcelSheet);
+router.route('/:id/excelsheetsend').post(protect, seller, sendExcelSheet);
 
 export default router;
